6b0225205cd4481ca7a72673669243172222e8e3
chunqishi <shicq@cs.brandeis.edu>
2014-05-14 17:27:25 -0400

    remove jsonpath dependency

--- src/main/java/edu/brandeis/cs/lappsgrid/util/JsonUtil.java
+++ src/main/java/edu/brandeis/cs/lappsgrid/util/JsonUtil.java
@@ -1,8 +1,5 @@
 package edu.brandeis.cs.lappsgrid.util;
 
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.ReadContext;
-import com.jayway.jsonpath.internal.JsonReader;
 import org.anc.lapps.serialization.Container;
 import org.json.JSONObject;
 import org.lappsgrid.api.Data;
@@ -39,16 +36,4 @@ public class JsonUtil
       String typeName = DiscriminatorRegistry.get(type);
       throw new LappsException("Unexpected Data object type: " + typeName);
    }
-
-   public static final String jsonpath(String json, String  path){
-       JsonPath jsonPath = JsonPath.compile(path);
-       ReadContext reader = new JsonReader().parse(json);
-       return reader.read(jsonPath).toString();
-   }
-
-
-   public static final boolean jsonEquals(String json, String other) {
-       return new JSONObject(json).toString().equals(new JSONObject(other).toString());
-   }
-
 }
